d414c6b9144a4eb3a2762cec9eed8c19f837739e
Razr <razr@razrsmacbook-pro.local>
2017-03-24 23:15:11 +0800

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Razr on 24/03/2017.
+//  Copyright © 2017 Razr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
